40bf1dde2ee85cb5502ed8fd12be2ddbd7003270
MadeSurya24 <sai007tdw@gmail.com>
2024-05-19 00:27:42 +0700

    .

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RCB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg" style="background:rgb(220, 0, 0)">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="img/logo&slide/logo.png" alt="RCB" width="250" height="50">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <form class="d-flex mx-auto"  role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light" type="submit">
+      <i class='bx bx-search' style="color:palevioletred;"></i>
+      </button>
+    </form>
+      <ul class="navbar-nav ms-auto column-gap-3">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ABOUT US</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class='bx bx-cart fs-3'></i></a>
+        </li>
+        <li class="nav-item d-grid gap-.5 d-md-flex justify-content-md-end">
+            <button class="btn me-md-2" style="background: pink;" type="button">
+                <a href="#" class="btn btn-pink btn-sm" role="button" data-bs-toggle="button">REGISTRASI</a>
+            </button>
+               
+            <button class="btn btn-danger" type="button">
+                <a href="#" class="btn btn-danger btn-sm" role="button" data-bs-toggle="button">LOGIN</a>
+            </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
